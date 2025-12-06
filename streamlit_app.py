b41270a72dc7bed799c8c86d5c06e447d1a7075a
ritamatildes <ritammatildes@gmail.com>
2025-12-06 17:11:10 +0000

    Revise final message format in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,7 +153,7 @@ def get_analysis_for_address(address):
     # --- Return Final Message ---
     message = ""
     if final_class and out_pop and out_cirac_desc:
-        message = f"Apresenta um potencial **{final_class}**: neste concelho ({out_municipality}) residem {out_pop} pessoas, o risco de inundação é {out_cirac_desc} e, num raio de 500m, existem {out_poi_count} pontos de interesse."
+        message = f"A morada analizada localiza-se no concelho ({out_municipality}) onde residem {out_pop}. Apresenta um risco de inundação (CIRAC) {out_cirac_desc} e, num raio de 500m, existem {out_poi_count} pontos de interesse. Para a tomada de decisão, deve considerar que esta localização tem um potencial global {final_class}."
     else:
         message = "Não foi possível concluir a análise. Um ou mais dados (população, POIs) não foram encontrados para este local."
     
